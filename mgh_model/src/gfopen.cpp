2277c92393134f9ee522fb9b5f2b45b6db36a683
Shane Fiorenza <shane.fiorenza@colorado.edu>
2018-06-26 11:47:30 -0600

    Restructured curator code; renamed mutant tubulin
    Former-commit-id: 20ea30532011c181eb39dd483fe80bf03814706c

--- mgh_model/src/gfopen.cpp
+++ mgh_model/src/gfopen.cpp
@@ -13,8 +13,8 @@ FILE *gfopen(const char *file_name, const char *type)
     FILE *ptr;
 
     if ((ptr = fopen(file_name, type)) == NULL) {
-	fprintf(stderr, "Cannot open %s in gfopen\n", file_name);
-	exit(1);
+		fprintf(stderr, "Cannot open %s in gfopen\n", file_name);
+		exit(1);
     }
 
     return ptr;

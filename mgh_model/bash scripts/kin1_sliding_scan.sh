150f1a410352925803114779781ca703a81540b7
Shane Fiorenza <shane.fiorenza@colorado.edu>
2018-11-28 14:18:34 -0700

    Last commit with tubulin_hetero/hsk_model/etc.

--- mgh_model/bash scripts/kin1_sliding_scan.sh
+++ mgh_model/bash scripts/kin1_sliding_scan.sh
@@ -2,30 +2,20 @@
 echo START KINESIN-1 SLIDING SCAN
 cd ~
 cd Projects/overlap_analysis/mgh_model
-for k in `seq 16 20`;
+for k in {1,10,100,1000};
 do 
 	FILE_NAME="SlideScan"
 	FILE_NAME+="_"
-	XLINK_CONC=$(echo "scale=2; 0.2 * $k" | bc)
-	if [ $k -le 9 ]; then
+	K_TETH=$(echo "scale=2; 0.01 * $k" | bc)
+	if [ $k -le 99 ]; then
 		FILE_NAME+="0"
 	fi
-	FILE_NAME+="$XLINK_CONC"
+	FILE_NAME+="$K_TETH"
 	echo $FILE_NAME
 	echo RUNNING NEW SIM: filename is $FILE_NAME
 	# Update YAML files
-	yq w -i params.yaml xlinks.concentration $XLINK_CONC
+	yq w -i params_slidescan.yaml motors.k_tether_free $K_TETH
 	# Run sim for these parameter values
-	./sim params.yaml $FILE_NAME
-	# Remove unnecessary files after each sim (save mem.)
-	MOTOR_FORCE_FILE="$FILE_NAME"
-	MOTOR_FORCE_FILE+="_motor_force.file"
-	rm $MOTOR_FORCE_FILE
-	MOTOR_EXT_FILE="$FILE_NAME"
-	MOTOR_EXT_FILE+="_motor_extension.file"
-	rm $MOTOR_EXT_FILE
-	TOT_FORCE_FILE="$FILE_NAME"
-	TOT_FORCE_FILE+="_total_force.file"
-	rm $TOT_FORCE_FILE
+	./sim params_slidescan.yaml $FILE_NAME
 done
 echo END SCAN

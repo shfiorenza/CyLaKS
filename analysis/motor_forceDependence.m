6d500f0b62eae1c719d4ac8554f9a6676a99d7dc
Shane Fiorenza <shfiorenza@gmail.com>
2021-02-03 09:59:58 -0700

    Update plot style

--- analysis/motor_forceDependence.m
+++ analysis/motor_forceDependence.m
@@ -1,8 +1,8 @@
-
+%{
 clear variables;
 sim_name = 'run_motor_forceVel/k401_forceVel';
-applied_forces = [-5.25, -4.25, -3.25]; %[-6, -5, -4, -3, -2, -1, 0];
-seeds = [0]; % , 1, 2, 3];
+applied_forces = [-6:0.25:0];
+seeds = [0 , 1, 2, 3];
 
 exp_forces = [0, -1.0 : -0.5 : -6.0];
 exp_runlengths = [940, 770, 480, 500, 270, 330, 250, 150, 240, 120, 70, 50];
@@ -33,43 +33,46 @@ end
 
 % Run length plot %
 fig1 = figure();
-set(fig1, 'Position', [50, 50, 960, 600]);
+set(fig1, 'Position', [50, 50, 1.5*480, 1.5*360]);
 hold all
 % Plot sim data
 sim_run = errorbar(applied_forces, avg_runlengths, err_runlengths, 'o', ...
-    'MarkerSize', 8, 'LineWidth', 2);
+    'MarkerSize', 10, 'LineWidth', 2);
 sim_run.MarkerFaceColor = sim_run.MarkerEdgeColor;
 % Plot experimental data
-exp_run = errorbar(exp_forces, exp_runlengths, exp_err_runlengths, 'sq', ...
-    'MarkerSize', 8, 'LineWidth', 2);
+exp_run = errorbar(exp_forces, exp_runlengths, exp_err_runlengths, '^', ...
+    'MarkerSize',14, 'LineWidth', 2, 'Color', 	[0.8500, 0.3250, 0.0980]);
 exp_run.MarkerFaceColor = exp_run.MarkerEdgeColor;
 % Plot experimental fit 
 L_0 = 1120;         % in nm
 sigma_off = 2.0;    % in nm
 kbT = 4.114;        % in pN * nm
-exp_run_fit = fplot(@(f) L_0*exp(-abs(f) * sigma_off/kbT), [-6 0], 'LineWidth', 2);
-exp_run_fit.Color = exp_run.Color;
+exp_run_fit = fplot(@(f) L_0*exp(-abs(f) * sigma_off/kbT), [-6 0], ...
+    'LineWidth', 3, 'Color', [0.8500, 0.3250, 0.0980]);
 % Bring sim data to front
 uistack(sim_run, 'top');
-ax = gca;
-ax.FontSize = 12; 
+
 % Label axes, legend, etc. 
-xlabel('Applied force (pN)', 'FontSize', 14);
-ylabel('Run length (nm)', 'FontSize', 14);
+xlabel('Applied force (pN)');
+ylabel('Run length (nm)');
+set(gca, 'FontSize', 20);
 xlim([-6.5 0.5]);
 ylim([0 1400]);
-legend({'Experimental data', 'Experimental fit', 'Simulation data'}, ... 
-    'location', 'northwest', 'FontSize', 12);
+yticks([0 250 500 750 1000 1250]);
+legend({'Experiment - Andreasson et al.', 'Model - Andreasson et al.', 'Simulation'}, ... 
+    'location', 'northwest', 'FontSize', 14);
 
 fig2 = figure();
-set(fig2, 'Position', [75, 75, 960, 600]);
+set(fig2, 'Position', [75, 75, 1.5*480, 1.5*360]);
 hold all
 sim_vel = errorbar(applied_forces, avg_velocities, err_velocities, 'o', ...
-    'MarkerSize', 8, 'LineWidth', 2);
+    'MarkerSize', 10, 'LineWidth', 2);
 sim_vel.MarkerFaceColor = sim_vel.MarkerEdgeColor;
-exp_vel = errorbar(exp_forces, exp_velocities, exp_err_velocities, 'sq', ...
-    'MarkerSize', 8, 'LineWidth', 2);
+% Plot experimental velocity data
+exp_vel = errorbar(exp_forces, exp_velocities, exp_err_velocities, '^', ...
+    'MarkerSize', 14, 'LineWidth', 2, 'Color', 	[0.8500, 0.3250, 0.0980]);
 exp_vel.MarkerFaceColor = exp_vel.MarkerEdgeColor;
+% Plot experimental velocity fit
 d_step = 8.2;         % in nm
 F_i = 26;           % in pN
 k0_1 = 5000;         % in 1/s
@@ -81,14 +84,13 @@ k_1 = @(f) k0_1 * exp(f * sigma_1 / kbT);
 k_2 = k0_2; 
 k_3 = @(f) k0_3 * exp((f + F_i) * sigma_3 / kbT);
 exp_vel_fit = fplot(@(f) d_step*k_1(f)*k_2*k_3(f) / (k_1(f)*k_2 + k_3(f)*(k_1(f) + k_2)), ...
-    [-6 0], 'LineWidth', 2);
-exp_vel_fit.Color = exp_vel.Color;
+    [-6 0], 'LineWidth', 3, 'Color', [0.8500, 0.3250, 0.0980]);
+
 uistack(sim_vel, 'top');
-ax = gca;
-ax.FontSize = 16; 
-xlabel('Applied force (pN)', 'FontSize', 18);
-ylabel('Velocity (nm/s)', 'FontSize', 18);
+xlabel('Applied force (pN)');
+ylabel('Velocity (nm/s)');
+set(gca, 'FontSize', 20);
 xlim([-6.5 0.5]);
 ylim([0 900]);
-legend({'Experimental data', 'Fit to 3-state model', 'Simulation'}, ... 
+legend({'Experiment - Andreasson et al.', 'Model - Andreasson et al.', 'Simulation'}, ... 
     'location', 'northwest', 'FontSize', 14);

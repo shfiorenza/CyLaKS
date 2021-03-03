a4e22ab341e4f36afa48651c2dc3a0343cbf1ecf
Shane Fiorenza <shfiorenza@gmail.com>
2021-03-03 13:05:14 -0700

    Make plot style consistent with CyLaKS paper

--- analysis/motor_forceDependence.m
+++ analysis/motor_forceDependence.m
@@ -1,8 +1,8 @@
 %{
 clear variables;
 sim_name = 'run_motor_forceVel/k401_forceVel';
-applied_forces = [-6:0.25:0];
-seeds = [0 , 1, 2, 3];
+applied_forces = [-6:0.5:0];
+seeds = [0 1, 2, 3];
 
 exp_forces = [0, -1.0 : -0.5 : -6.0];
 exp_runlengths = [940, 770, 480, 500, 270, 330, 250, 150, 240, 120, 70, 50];
@@ -33,47 +33,48 @@ end
 
 % Run length plot %
 fig1 = figure();
-set(fig1, 'Position', [50, 50, 1.5*480, 1.5*360]);
+set(fig1, 'Position', [50, 50, 720, 540]);
 hold all
 % Plot sim data
-sim_run = errorbar(applied_forces, avg_runlengths, err_runlengths, 'o', ...
-    'MarkerSize', 10, 'LineWidth', 2);
+sim_run = errorbar(applied_forces, avg_runlengths/1000, err_runlengths/1000, 'o', ...
+    'MarkerSize', 12, 'LineWidth', 2);
 sim_run.MarkerFaceColor = sim_run.MarkerEdgeColor;
 % Plot experimental data
-exp_run = errorbar(exp_forces, exp_runlengths, exp_err_runlengths, '^', ...
-    'MarkerSize',14, 'LineWidth', 2, 'Color', 	[0.8500, 0.3250, 0.0980]);
+exp_run = errorbar(exp_forces, exp_runlengths/1000, exp_err_runlengths/1000, '^', ...
+    'MarkerSize',12, 'LineWidth', 2, 'Color', 	[0.6 0.6 0.6]);
 exp_run.MarkerFaceColor = exp_run.MarkerEdgeColor;
 % Plot experimental fit 
-L_0 = 1120;         % in nm
+L_0 = 1.120;         % in um
 sigma_off = 2.0;    % in nm
 kbT = 4.114;        % in pN * nm
 exp_run_fit = fplot(@(f) L_0*exp(-abs(f) * sigma_off/kbT), [-6 0], ...
-    'LineWidth', 3, 'Color', [0.8500, 0.3250, 0.0980]);
+    'LineWidth', 2, 'Color', [0.6 0.6 0.6]);
 % Bring sim data to front
 uistack(sim_run, 'top');
 
 % Label axes, legend, etc. 
-xlabel('Applied force (pN)');
-ylabel('Run length (nm)');
-set(gca, 'FontSize', 20);
+xlabel('Applied force (pN)', 'FontSize', 22);
+ylabel('Run length (um)', 'FontSize', 22);
+set(gca, 'FontSize', 22);
 xlim([-6.5 0.5]);
-ylim([0 1400]);
-yticks([0 250 500 750 1000 1250]);
-legend({'Experiment - Andreasson et al.', 'Model - Andreasson et al.', 'Simulation'}, ... 
-    'location', 'northwest', 'FontSize', 14);
+ylim([0 1.4]);
+yticks([0 0.3 0.6 0.9 1.2]);
+legend({'Experiment (Ref. X)', 'Model (Ref. X)', 'Simulation'}, ... 
+    'location', 'northwest', 'FontSize', 20);
+legend('boxoff');
 
 fig2 = figure();
-set(fig2, 'Position', [75, 75, 1.5*480, 1.5*360]);
+set(fig2, 'Position', [75, 75, 720, 540]);
 hold all
-sim_vel = errorbar(applied_forces, avg_velocities, err_velocities, 'o', ...
-    'MarkerSize', 10, 'LineWidth', 2);
+sim_vel = errorbar(applied_forces, avg_velocities/1000, err_velocities/1000, 'o', ...
+    'MarkerSize', 12, 'LineWidth', 2);
 sim_vel.MarkerFaceColor = sim_vel.MarkerEdgeColor;
 % Plot experimental velocity data
-exp_vel = errorbar(exp_forces, exp_velocities, exp_err_velocities, '^', ...
-    'MarkerSize', 14, 'LineWidth', 2, 'Color', 	[0.8500, 0.3250, 0.0980]);
+exp_vel = errorbar(exp_forces, exp_velocities/1000, exp_err_velocities/1000, '^', ...
+    'MarkerSize', 12, 'LineWidth', 2, 'Color', 	[0.6 0.6 0.6]);
 exp_vel.MarkerFaceColor = exp_vel.MarkerEdgeColor;
 % Plot experimental velocity fit
-d_step = 8.2;         % in nm
+d_step = 0.008;         % in um
 F_i = 26;           % in pN
 k0_1 = 5000;         % in 1/s
 sigma_1 = 4.6;      % in nm
@@ -84,13 +85,13 @@ k_1 = @(f) k0_1 * exp(f * sigma_1 / kbT);
 k_2 = k0_2; 
 k_3 = @(f) k0_3 * exp((f + F_i) * sigma_3 / kbT);
 exp_vel_fit = fplot(@(f) d_step*k_1(f)*k_2*k_3(f) / (k_1(f)*k_2 + k_3(f)*(k_1(f) + k_2)), ...
-    [-6 0], 'LineWidth', 3, 'Color', [0.8500, 0.3250, 0.0980]);
+    [-6 0], 'LineWidth', 2, 'Color', [0.6 0.6 0.6]);
 
 uistack(sim_vel, 'top');
-xlabel('Applied force (pN)');
-ylabel('Velocity (nm/s)');
-set(gca, 'FontSize', 20);
+set(gca, 'FontSize', 22);
+xlabel('Applied force (pN)', 'FontSize', 22);
+ylabel('Velocity (nm/s)', 'FontSize', 22);
 xlim([-6.5 0.5]);
-ylim([0 900]);
-legend({'Experiment - Andreasson et al.', 'Model - Andreasson et al.', 'Simulation'}, ... 
-    'location', 'northwest', 'FontSize', 14);
+ylim([0 0.9]);
+%legend({'Experiment - Andreasson et al.', 'Model - Andreasson et al.', 'Simulation'}, ... 
+ %   'location', 'northwest', 'FontSize', 14);
--- analysis/plot_mobility_heterodimer.m
+++ analysis/plot_mobility_heterodimer.m
@@ -1,13 +1,13 @@
 %{
 clear variables;
 baseNames = ["hetero_tubulin"];
-folder = "run_hetero_tubulin";
+folder = "run_tubulin_mobility";
 
 dir = sprintf("/home/shane/projects/CyLaKS/%s", folder);
 
 color_exp = [0, 0.4470, 0.7410]; %[1 0 0]; %[0.55 0.55 0.55];
 
-p_hetero = [0.0, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 1.0];
+p_hetero = [0.0, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 1.0];
 seeds = [0, 1, 2, 3, 4, 5];
 
 n_fractions = length(p_hetero);
@@ -38,34 +38,40 @@ fig1 = figure();
 set(fig1, 'Position', [50, 50, 1400, 350])  % For 3 subplots
 
 subplot(1, 3, 1) 
-exp_data = errorbar(p_hetero, runlengths, err_runlengths, 'o', ...
+hold on
+set(gca, 'FontSize', 20);
+exp_data = errorbar(p_hetero, runlengths/1000, err_runlengths/1000, 'o', ...
     'MarkerSize', 8, 'LineWidth', 2, 'MarkerEdgeColor', color_exp);
 exp_data.MarkerFaceColor = exp_data.MarkerEdgeColor;
 exp_data.Color = exp_data.MarkerEdgeColor;
-ylabel('Run Length (nm)', 'FontSize', 14);
-set(gca, 'FontSize', 14);
+ylabel('Run Length (\mum)', 'FontSize', 20);
 xlim([-0.05 1.05]);
-ylim([0 1800]);
+ylim([0 13]);
+yticks([0 4 8 12]);
 
 subplot(1, 3, 2 ) %4, 2)
+hold on
+set(gca, 'FontSize', 20);
 exp_data = errorbar(p_hetero, lifetimes, err_lifetimes, 'o', ... 
     'MarkerSize', 8, 'LineWidth', 2, 'MarkerEdgeColor', color_exp);
 exp_data.MarkerFaceColor = exp_data.MarkerEdgeColor;
 exp_data.Color = exp_data.MarkerEdgeColor;
-xlabel('Fraction of sites that are mutant', 'FontSize', 14);
-ylabel('Lifetime (s)', 'FontSize', 14);
-set(gca, 'FontSize', 14);
+xlabel('Fraction of sites that are mutant', 'FontSize', 20);
+ylabel('Lifetime (s)', 'FontSize', 20);
 xlim([-0.05 1.05]);
-ylim([0 3]);
+ylim([0 23]);
+yticks([0 7 14 21]);
 
 subplot(1, 3, 3) %4, 3)
-exp_data = errorbar(p_hetero, velocities, err_velocities, 'o', ...
+hold on
+set(gca, 'FontSize', 20);
+exp_data = errorbar(p_hetero, velocities/1000, err_velocities/1000, 'o', ...
     'MarkerSize', 8, 'LineWidth', 2, 'MarkerEdgeColor', color_exp);
 exp_data.MarkerFaceColor = exp_data.MarkerEdgeColor;
 exp_data.Color = exp_data.MarkerEdgeColor;
-ylabel('Velocity (nm/s)', 'FontSize', 14);
-set(gca, 'FontSize', 14);
+ylabel('Velocity (\mum/s)', 'FontSize', 20);
 xlim([-0.05 1.05]);
-ylim([0 700]);
+ylim([0 1]);
+yticks([0 0.3 0.6 0.9]);
 
 %suptitle('Kinesin mobility with mutant sites (k_D = 4k^0_D) randomly inserted')
